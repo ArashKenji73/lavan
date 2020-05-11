f5d126e6de78d05218361c247ec022f717b6ace9
arashkenji73 <kenjiarash@gmail.com>
2020-05-11 21:12:12 +0430

    fix line animation navbar

---
+++ assets/js/sampleSlider.js
@@ -0,0 +1,12 @@
+// its for main slider 
+var flkty = new Flickity('#sampleCarousel', {
+    //these are options
+    cellAlign: "left",
+    //wrapAround: true,
+    rightToLeft: true,
+    //draggable: false,
+    contain: true,
+    groupCells: true,
+    // disable previous & next buttons and dots
+    prevNextButtons: false,
+});
