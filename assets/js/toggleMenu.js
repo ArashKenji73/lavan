716be354149c97e4283cf007bdbf09d70d540052
arashkenji73 <kenjiarash@gmail.com>
2020-05-11 10:54:34 +0430

    add active line to menu

--- assets/js/toggleMenu.js
+++ assets/js/toggleMenu.js
@@ -1,51 +1,41 @@
-$(document).ready(function() {
-    $("#toggleMenu").on("click", function(){
+$(document).ready(function () {
+    $("#toggleMenu").on("click", function () {
         $('nav').toggleClass('toggledIn')
     });
-    
-    
-    $("#closeMenu").on("click", function(){
+
+
+    $("#closeMenu").on("click", function () {
         $('nav').removeClass('toggledIn')
     });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //add blury effect to menu items
-    $('.main-nav li').hover(function(e){
-        
-        $('.main-nav li').css('filter' , 'blur(3px)');
-        
-        
-        $(e.target.parentElement).css({
-            'filter' :'blur(0px)',
-            'transform' :'scale(1.5)',
-        });     
-    },function(){
-        $(this).css({
-            'transform' :'scale(1)',
-        }); 
-    });
-    
-    
-    
-    $('.main-nav li').mouseleave(function(){
-        $('.main-nav li').css('filter' , 'blur(0px)');
-        
-        
-        
-    })
 
-    
-    
-    
+
+
+
+
+
+
+
+
+//add blury effect to menu items
+$('.main-nav li').hover(function (e) {
+
+    $('.main-nav li').css('filter', 'blur(2px)');
+
+
+    $(e.target.parentElement).css({
+        'filter': 'blur(0px)',
+        'transform': 'scale(1.1)',
+    });
+}, function () {
+    $(this).css({
+        'transform': 'scale(1)',
+    });
 });
 
 
 
+$('.main-nav li').mouseleave(function () {
+    $('.main-nav li').css('filter', 'blur(0px)');
+});
 
+});
