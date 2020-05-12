a7bde5278df67c98cfc8bdeef375dca1450560a3
arashkenji73 <kenjiarash@gmail.com>
2020-05-12 10:21:15 +0430

    fix scale navbar

--- assets/js/toggleMenu.js
+++ assets/js/toggleMenu.js
@@ -17,25 +17,44 @@ $(document).ready(function () {
 
 
 //add blury effect to menu items
-$('.main-nav li').hover(function (e) {
+$('.main-nav li a').hover(function (e) {
 
+//    $('.main-nav li').css('filter', 'blur(2px)');
+//
+//
+//    $(e.target.parentElement).css({
+//        'filter': 'blur(0px)',
+//        'transform': 'scale(1.1)',
+//    });
+//}, function () {
+//    $(this).css({
+//        'transform': 'scale(1)',
+//    });
+//});
+//
+//
+//
+//$('.main-nav li').mouseleave(function () {
+//    $('.main-nav li').css('filter', 'blur(0px)');
+    
+    //$('.main-nav li').mouseleave(function () {
+//    $('.main-nav li').css('filter', 'blur(0px)');
+    
+    
     $('.main-nav li').css('filter', 'blur(2px)');
-
-
-    $(e.target.parentElement).css({
+    $(this.parentElement).css({
         'filter': 'blur(0px)',
         'transform': 'scale(1.1)',
     });
-}, function () {
-    $(this).css({
-        'transform': 'scale(1)',
-    });
-});
-
-
-
-$('.main-nav li').mouseleave(function () {
+    
+    
+    
+},function(){
     $('.main-nav li').css('filter', 'blur(0px)');
+    $('.main-nav li').css('transform', 'scale(1)');
+    
+    
+    
 });
 
 });
